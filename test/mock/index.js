d1636b9c4aab21badc5f57e2c624b3e5321c07f2
Arthur Chaloin <arthur@achaloin.com>
2019-05-03 16:33:02 +0200

    feat(tests): add login/auth capabilities to mock server

--- test/mock/index.js
+++ test/mock/index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const fs = require('fs');
+const md5 = require('md5');
 
 const app = express();
+const userpass = 'manage_!manage';
 const endpoints = [
   'create/vdisk/level/r5/disks/2.6,2.7,2.8/vd-1',
   'invalid/xml',
   'status/code/1',
 ];
 
-endpoints.forEach(endpoint => app.get(`/api/${endpoint}`, (_, res) => {
+app.get('/api/login/:token', (req, res) => {
+  if (md5(userpass) === req.params.token) {
+    res.send(fs.readFileSync(`data/login.xml`));
+    return;
+  }
+
+  res.status(401).send('nope');
+});
+
+endpoints.forEach(endpoint => app.get(`/api/${endpoint}`, (req, res) => {
   let rawData;
   let path = endpoint.split('/');
+
+  if (req.headers['sessionkey'] !== 'HereIsTheToken') {
+    res.status(401).send('nope');
+    return;
+  }
 
   while (path.length) {
     try {
