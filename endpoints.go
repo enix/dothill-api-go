8b20879b2327a2691f194034f9ed9d40a73e8c65
Arthur Chaloin <arthur@achaloin.com>
2019-05-07 18:03:08 +0200

    feat(endpoints): implement create volume and map volume routes

--- endpoints.go
+++ endpoints.go
@@ -25,3 +25,13 @@ func (client *Client) TestCall() (*TestModel, *ResponseStatus, error) {
 	status, err := client.requestAndConvert(res, "/create/vdisk/level/r5/disks/2.6,2.7,2.8/vd-1")
 	return res, status, err
 }
+
+// CreateVolume : creates a volume with the given name, capacity in the given pool
+func (client *Client) CreateVolume(name, size, pool string) (*Response, *ResponseStatus, error) {
+	return client.Request(fmt.Sprintf("/create/volume/pool/\"%s\"/size/%s/tier-affinity/no-affinity/\"%s\"", pool, size, name))
+}
+
+// MapVolume : map a volume to host + LUN
+func (client *Client) MapVolume(name, host string, lun int) (*Response, *ResponseStatus, error) {
+	return client.Request(fmt.Sprintf("/map/volume/access/rw/lun/%d/host/%s/\"%s\"", lun, host, name))
+}
