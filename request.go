248496f009c5c0c4940f80c21767f5483c3ab100
Arthur Chaloin <arthur@achaloin.com>
2019-06-19 14:03:05 +0200

    fix(client): refresh login when session key expires

--- request.go
+++ request.go
@@ -12,23 +12,25 @@ type Request struct {
 	Data     interface{}
 }
 
-func (req *Request) execute(client *Client) ([]byte, error) {
+func (req *Request) execute(client *Client) ([]byte, int, error) {
 	url := fmt.Sprintf("%s/api%s", client.Addr, req.Endpoint)
 	httpReq, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 
+	fmt.Println(client.sessionKey)
 	httpReq.Header.Set("sessionKey", client.sessionKey)
 	res, err := client.HTTPClient.Do(httpReq)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
 
 	if res.StatusCode >= 400 {
-		return nil, fmt.Errorf("API returned unexpected HTTP status %d", res.StatusCode)
+		return nil, res.StatusCode, fmt.Errorf("API returned unexpected HTTP status %d", res.StatusCode)
 	}
 
 	defer res.Body.Close()
-	return ioutil.ReadAll(res.Body)
+	data, err := ioutil.ReadAll(res.Body)
+	return data, res.StatusCode, err
 }
